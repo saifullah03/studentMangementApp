c27082c5cf8aff26a38348121597e05cd294ea43
SUCCESS <khalid.saifullah0012@gmail.com>
2024-09-10 10:00:55 +0600

    My first push

---
+++ src/main/java/com/microtech/studentmanagementappnew/StudentManagementAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.microtech.studentmanagementappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementAppNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementAppNewApplication.class, args);
+    }
+
+}
